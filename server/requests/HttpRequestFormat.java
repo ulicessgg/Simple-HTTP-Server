c60202cbd5ab24b75099b72e5a2c2196ce39d7c7
Ulices Gonzalez <sergioulicesgonzalezguerrero@gmail.com>
2025-02-20 23:47:13 -0800

    implemented parse in httprequestformat as per comments

--- server/requests/HttpRequestFormat.java
+++ server/requests/HttpRequestFormat.java
@@ -1,5 +1,10 @@
 package server.requests;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.regex.Matcher; // help parsing 
+import java.util.regex.Pattern;
+
 public class HttpRequestFormat {
     private HttpRequestLine requestLine;
 
@@ -23,16 +28,69 @@ public class HttpRequestFormat {
     }
 
     // TODO: must parse format. this is for when we want the server to read the info
-    // public static HttpRequestFormat parse(BufferedReader reader) throws IOException {
-    //     // part 1: initialize first part (method path version\r\n) w/ carriage return
+    public static HttpRequestFormat parse(BufferedReader reader) throws IOException 
+    {
+        try 
+        {
+            // part 1: initialize first part (method path version\r\n) w/ carriage return
+            String line = reader.readLine();
+            if(line == null)
+            {
+                return null;    // client error
+            }
 
-    //     // part 2: initialize second part (Map<String, String> headers \r\n)
+            String[] lineSegments = line.split(" ");
+            if(lineSegments.length != 3)
+            {
+                return null;    // line is invalid
+            }
 
-    //     // part 3: initialize the third part (\r\n)
+            HttpRequestLine requestLine = new HttpRequestLine(lineSegments[0], lineSegments[1]);
 
-    //     // part 4: initialize the fourth part (if given). note no carriage return
+            // part 2: initialize second part (Map<String, String> headers \r\n)
+            HttpRequestHeaders header = new HttpRequestHeaders();
+            while((line = reader.readLine()) != null && !line.isEmpty())
+            {
+                // part 3: initialize the third part (\r\n)
+                int index = line.indexOf(": ");
+                if(index > 0)
+                {
+                    header.addHeader(line.substring(0, index), line.substring(index + 2));
+                }
+            }
 
-    // }
+            // part 4: initialize the fourth part (if given). note no carriage return
+            String conLenStr = header.getHeader("Content-Length");
+            StringBuilder body = new StringBuilder();
+            if(conLenStr != null)
+            {
+                try
+                {
+                    int contentLength = Integer.parseInt(conLenStr);
+                    for(int i = 0; i < contentLength; i++)
+                    {
+                        int temp = reader.read();
+                        if(temp == -1)
+                        {
+                            break;  // eof
+                        }
+                        body.append((char) temp);
+                    }
+                }
+                catch(NumberFormatException e)
+                {
+                    return null; // content length invalid
+                }
+            }
+
+            return new HttpRequestFormat(requestLine, header, body.toString());
+        }
+
+        catch (IOException e) 
+        {
+        return null;
+        }
+    }
 
     public HttpRequestLine getRequestLine() {
         return requestLine;
