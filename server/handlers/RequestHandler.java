0b32a9248a87654b805ae8b895332578068604f2
Ulices Gonzalez <sergioulicesgonzalezguerrero@gmail.com>
2025-02-24 17:31:48 -0800

    implemented more of handlerequest and can work hard coded but need to test once we have responses implemented. edited requesthandler and add constructor since i forgot to and didnt have it properly used in webserver. deleted data dir since we can use the test site now.

--- server/handlers/RequestHandler.java
+++ server/handlers/RequestHandler.java
@@ -5,14 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files; // leave these we need em to access requested files
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import server.config.MimeTypes;
 import server.requests.HttpRequestFormat;
 import server.requests.HttpRequestHeaders;
 import server.requests.HttpRequestLine;
 
 public class RequestHandler 
 {
+    private String documentRoot;
+    private MimeTypes mimeTypes;
+
+    // added constructor as handler essentially had no info to work off of besides the socket port
+    public RequestHandler(String documentRoot, MimeTypes mimeTypes)
+    {
+        this.documentRoot = documentRoot;
+        this.mimeTypes = mimeTypes;
+    }
+    
     // this is for when we want the server to read the info
-    public static HttpRequestFormat parse(BufferedReader reader) throws IOException 
+    public HttpRequestFormat parse(BufferedReader reader) throws IOException 
     {
         try 
         {
@@ -76,30 +91,70 @@ public class RequestHandler
         }
     }
 
-    // gonna try and implement using comment logic for parse() in HttpRequestFormat.java WORK IN PROGRESS
-    public static void handleRequest(Socket clientSocket)
+    // works now if hard coding responses, left out to implement dynamic
+    public void handleRequest(Socket clientSocket)
     {
         try(clientSocket; 
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader in = new BufferedReader(
+                                new InputStreamReader(clientSocket.getInputStream()));
             OutputStream out = clientSocket.getOutputStream())
+        {
+            //if it works then the returned value should be valid any bad requests left as null
+            HttpRequestFormat request = HttpRequestFormat.parse(in);
+            if(request != null)
             {
-                HttpRequestFormat request = HttpRequestFormat.parse(in); // parse isnt implemented so may need to rewrite or adjust this, if it works then the returned value should be valid
-                                                                         // and any bad requests will essentially be left as null
-                // valid requests will then be handled accordingly 
-                if(request != null)
+                // gather info from request to find the path to a file
+                String file = request.getRequestLine().getPath();
+                String pathToFile = documentRoot + file;
+                Path path = Paths.get(pathToFile);
+
+                if (Files.exists(path) && !Files.isDirectory(path)) // check if it even exists
                 {
-                    // wait till parse is implemented in order to make sure this can be attempted
-                }
-                else    // invalid will result in a 400
+                    // leaving this out for now since not all requests require data output
+                    // byte[] data = Files.readAllBytes(path);
+
+                    // implemented helper below and using mimehelper gets us the file type
+                    String extension = getExtension(file);
+                    String fileType = mimeTypes.getMimeTypeFromExtension(extension);
+
+                    String response = ""; // leaving blank need to figure out dynamic responses
+
+                    out.write(response.getBytes());
+                    // leaving this out for now since not all requests require data output
+                    // out.write(data);
+                    out.flush();
+                } 
+                else    
                 {
-                    String error = String.format("HTTP/1.1 %d %s\r\n\r\n", 400, "Bad Request");
+                    String error = ""; // leaving blank need to figure out dynamic responses
                     out.write(error.getBytes());
                     out.flush();
                 }
             }
-        catch (IOException e)
+            else    // invalid will result in a 400
             {
-                System.err.println();
+                String error = ""; // leaving blank need to figure out dynamic responses
+                out.write(error.getBytes());
+                out.flush();
             }
+        }
+        catch (IOException e)
+        {
+            System.err.println();   // need to figure out what to say in print
+        }
+    }
+
+    public String getExtension(String file) 
+    {
+        // finds the . in a given file name, used lastIndex just incase the name is or has a .
+        int dot = file.lastIndexOf(".");
+        if (dot > 0 && dot < file.length() - 1) // this just makes sure the . is in the right spot
+        {
+            return file.substring(dot + 1); // this would be the extension
+        }
+        else
+        {
+            return null;
+        }
     }
 }
